0bc8ae8aedc688bfc56fcd5df8c14b9bedab58d8
Ankit Mod <ankit300mod@gmail.com>
2024-07-16 18:27:08 +0530

    new things added

--- src/components/PreviousOrders.jsx
+++ src/components/PreviousOrders.jsx
@@ -1,42 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { BackendURl } from '../Helper/Helper';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const PreviousOrders = () => {
+const PreviousOrders = () => { 
   const [data, setData] = useState([]);
   const eml = localStorage.getItem('email');
-
+    
   const PreviousOrder = async () => {
     try {
-      const response = await axios.get('https://food-app-backend-peach.vercel.app/api/v1/previousorder', {
-        eml
+      const response = await axios.post('https://food-app-backend-peach.vercel.app/api/v1/previousorder', {
+        email: eml
       });
-        setData(response.data); 
-        console.log(response);
+      setData(response.data.data); 
+      console.log(response.data.data);
     } catch (error) {
       console.error('Error fetching previous orders:', error.message);
     }
   };
 
-  useEffect(() => {
+  useEffect(() => { 
     PreviousOrder();
   }, []);
 
   return (
-    <div>
-      <h1>Previous Orders</h1>
+    <div className="container mt-4">
+      <h1 className="text-center mb-4">Previous Orders</h1>
       {data.length === 0 ? (
-        <p>No previous orders found.</p>
+        <p className="text-center">No previous orders found.</p>
       ) : (
         data.map((prev) => (
-          <div key={prev._id}>
-            <p>Email: {prev.email}</p>
-            <p>Order Data:</p>
-            <ul>
-              {prev.order_data.map((item, index) => (
-                <li key={index}>{item}</li>
-              ))}
-            </ul>
+          <div className="card mb-3" key={prev._id}>
+            <div className="card-header">
+              <h5>{prev.email}</h5>
+            </div>
+            <div className="card-body">
+                    {prev.order_data.map((item, index) => {
+                    const formattedDate = new Date(item.date).toLocaleDateString();
+
+                       return <div className="row mb-2" key={index}>
+                            <div className="col-sm-4"><strong>Name:</strong> {item.name}</div>
+                            <div className="col-sm-4"><strong>Quantity:</strong> {item.quantity}</div>
+                            <div className="col-sm-4"><strong>Price:</strong> {item.price}</div>
+                            <div className='col-sm-4'><strong>Date:</strong>{formattedDate}</div>
+                        </div>
+                    })}
+            </div>
           </div>
         ))
       )}
